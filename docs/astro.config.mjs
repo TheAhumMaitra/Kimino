0671bb47df1cef701c5bc3d846119fad8e15edf6
Ahum Maitra <theahummaitra@gmail.com>
2026-02-09 16:35:28 +0530

    feat: Add docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Kimino',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/TheAhumMaitra/Kimino' }],
+			sidebar: [
+				{
+					label: 'Documentation',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Introduction', slug: 'guides/introduction' },
+						{ label: 'Installation', slug: 'guides/installation' },
+					],
+				}
+			],
+		}),
+	],
+});
---
+++ docs/src/content/docs/guides/introduction.md
@@ -0,0 +1,7 @@
+---
+title: Introduction to Kimino
+description: An introduction to Kimino
+---
+
+
+Hello, Welcome to Kimino! I made Kimino for beginners and creative thinkers. Let's say, you want to create a web app (like: Youtube) to launch from your App viewer in Linux. Maybe you just want to create a desktop app entry for a new app, you just downloaded. Don't worry, if you want to create multiple desktop app entry. Kimino is specifically made for these situations. With Kimino you can create your customized app entries.
